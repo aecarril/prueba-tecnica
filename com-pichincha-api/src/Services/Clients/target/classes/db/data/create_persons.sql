6b026330fd1d90031e24d2a31e509f341ca67148
aecarril <aecarril@gmail.com>
2023-11-05 19:04:25 -0500

    Delete com-pichincha-api/src/Services/Clients/target directory

--- com-pichincha-api/src/Services/Clients/target/classes/db/data/create_persons.sql
+++
@@ -1,9 +0,0 @@
-/*
--- Query: SELECT * FROM clientsdb.PERSONS
-LIMIT 0, 1000
-
--- Date: 2023-11-04 22:20
-*/
-INSERT INTO `PERSONS` (`identification`,`names`,`gender`,`age`,`address`,`phone`) VALUES ('0201288479','Marianela Montalvo','Femenino',34,'Amazonas y NNUU','097548965');
-INSERT INTO `PERSONS` (`identification`,`names`,`gender`,`age`,`address`,`phone`) VALUES ('1713526108','Juan Osorio','Masculino',25,'13 junio y Equinoccial','098874587');
-INSERT INTO `PERSONS` (`identification`,`names`,`gender`,`age`,`address`,`phone`) VALUES ('1713526109','Jose Lema','Masculino',45,'Otavalo sn y principal','098254785');
